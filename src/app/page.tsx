5abb2429d3489f62d067f6b4b74b76259dc4346d
bojidaryovchev <blizzardcon10@gmail.com>
2025-06-11 01:11:29 +0300

    fix: proper imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import About from "@/components/about";
+import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import Gallery from "@/components/gallery";
 import Hero from "@/components/hero";
 import Services from "@/components/services";
-import { Contact } from "lucide-react";
 
 const Home: React.FC = () => {
   return (
