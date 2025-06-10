f51c1ad069e91f98d53adcb86a4f348711d8f004
bojidaryovchev <blizzardcon10@gmail.com>
2025-06-11 01:18:10 +0300

    feat: favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,15 @@ export const metadata: Metadata = {
     description: `Довършителни ремонти, гипсов картон, окачени тавани, шпакловки, латекс и много други услуги`,
     type: "website",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
